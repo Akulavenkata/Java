881e19208053856f71c06a71a5fcbb1bd5f0c594
Akulavenkata <151906080+Akulavenkata@users.noreply.github.com>
2024-08-23 23:01:49 +0530

    Create Addition1.java

---
+++ Addition1.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Addition1 {
+    public static void main(String args[]) {
+        int x=5,y=10,z;
+        z=x+y;
+        System.out.println(" Sum of " +x+ " and " +y+ " is " +z);
+    }
+}
