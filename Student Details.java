efb705748d966e80a170db079a2c4df0dd6eee13
Akulavenkata <151906080+Akulavenkata@users.noreply.github.com>
2024-08-23 21:15:27 +0530

    Create Student Details.java

---
+++ Student Details.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class StudentDetails {
+    public static void main(String args[]) {
+    System.out.println("\t\t\t\t\tStudentDetails");
+    System.out.println("Name:Akula.VenkataSatya\t\tRegNo:23K61A4702");
+    System.out.println("FatherName:akula.Tatarao\tAadharNo:233456788765");
+    System.out.println("Occupation:Welder\t\t\tDOB:14/04/2005");
+    System.out.println("parentMobileNo:8978153312\tNo.of subjects passed:10");
+}
+}
