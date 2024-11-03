b4b16aa2bea7729d5b2361bfb5469e65593997cc
Akulavenkata <151906080+Akulavenkata@users.noreply.github.com>
2024-11-03 23:30:59 +0530

    Create MatrixMultiplication.java

---
+++ MatrixMultiplication.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class MatrixMultiplication {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the elements of rows and columns in matrix A : ");
+        int r1=sc.nextInt();
+        int c1=sc.nextInt();
+        System.out.println("Enter the elements of rows and columns in matrix B : ");
+        int r2=sc.nextInt();
+        int c2=sc.nextInt();
+        if(r1!=c2) {
+            System.out.println("Multiplication is not possible ");
+            System.exit(0);
+        }
+        else {
+            int a[][]=new int[r1][c1];
+            int b[][]=new int[r2][c2];
+            int c[][]=new int[r1][c2];
+            System.out.println(" Enter " +(r1*c1)+ " in to matrix A ");
+            for(int i=0;i<r1;i++) {
+                for(int j=0;j<c1;j++) {
+                    a[i][j]=sc.nextInt();
+                }
+            }
+            System.out.println(" Enter " +(r2*c2)+ " in to matrix B ");
+            for(int i=0;i<r2;i++) {
+                for(int j=0;j<c2;j++) {
+                b[i][j]=sc.nextInt();    
+                }
+            }
+            for(int i=0;i<r1;i++) {
+                for(int j=0;j<c1;j++) {
+                    c[i][j]=0;
+                    for(int k=0;k<c1;k++) {
+                    c[i][j]=+c[i][j]+(a[i][j]*b[i][k]);
+                    }
+                }
+            }
+            System.out.println("the resultant matrix C is : ");
+            for(int i=0;i<r1;i++) {
+                for(int j=0;j<c2;j++) {
+                    System.out.print(c[i][j]+" ");
+                }
+                System.out.println(" ");
+            }
+        }
+    }
+}    
